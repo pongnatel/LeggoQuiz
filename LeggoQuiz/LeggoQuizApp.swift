f1b5b4448af7193a6e169a49aca73900edfef3cd
pongnatel <s3877819@rmit.edu.vn>
2023-11-23 18:41:31 +0700

    Initial Commit

---
+++ LeggoQuiz/LeggoQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeggoQuizApp.swift
+//  LeggoQuiz
+//
+//  Created by hhhh on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct LeggoQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
